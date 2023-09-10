887f174145015db02d8664928c171a8411f61f14
kelley <136962588+mskelleygvt@users.noreply.github.com>
2023-09-09 20:50:45 -0700

    No change

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 
-export default function Header({ name }) {
+export default function Header({ Kelley }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl dark:text-white text-center">
-        <Link href="/">
-          <a>{name}</a>
+        <Link href="*.">
+          <a>{Felicia Ann Kelley}</a>
         </Link>
       </p>
     </header>
